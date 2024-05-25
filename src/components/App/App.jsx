f727de0a5ef6f5ad9d5cb10e24dd963f93067ee2
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-25 16:58:34 +0300

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "modern-normalize";
+import Navigation from "../Navigation/Navigation";
+import HomePage from "../../pages/HomePage/HomePage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
+import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import MovieCast from "../MovieCast/MovieCast";
+import MovieReviews from "../MovieReviews/MovieReviews";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />}>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+          <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+}
