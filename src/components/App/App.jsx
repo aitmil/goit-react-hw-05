9eb22eb316a27cdcf8b56523856dcb59bfbc9e30
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-26 22:13:40 +0300

    add lazy & navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,33 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import "modern-normalize";
-import Navigation from "../Navigation/Navigation";
-import HomePage from "../../pages/HomePage/HomePage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+import { Navigation } from "../Navigation/Navigation";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
 import css from "./App.module.css";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
 
 export default function App() {
   return (
     <div className={css.container}>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />}>
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-          <Route path="/movies/:movieId/cast" element={<MovieCast />} />
-          <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />}>
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+            <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+            <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,21 @@
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 import css from "./Navigation.module.css";
 
-export default function Navigation() {
-  return <></>;
-}
+export const Navigation = () => {
+  const getLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+  return (
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <NavLink to="/" className={getLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={getLinkClass}>
+          Movies
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
