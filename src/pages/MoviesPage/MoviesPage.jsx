b7d8be14c0bf7f3a0cdbf3fb2d5b1bb091c6426c
Anna Taranchuk <taranchuk4@yahoo.com>
2024-05-28 00:24:58 +0300

    update MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState, useRef, Suspense } from "react";
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLocation,
+  useParams,
+} from "react-router-dom";
+import toast from "react-hot-toast";
+import { getMovieDetails } from "../../movies-api";
 import css from "./MoviesPage.module.css";
 
 export default function MoviesPage() {
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchMovieById(movieId) {
+      setIsLoading(true);
+      try {
+        const data = await getMovieDetails(movieId);
+        setMovie(data);
+      } catch (error) {
+        toast("Whoops. Something went wrong! Please try to reload this page!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchMovieById();
+  }, []);
+
   return <></>;
 }
